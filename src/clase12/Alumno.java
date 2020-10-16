531a7b9874709c754eb2de1b95d4c6b3d0887141
herfigueroa <hernan.fig60@gmail.com>
2020-10-16 17:16:21 -0300

    Commit inicial

---
+++ src/clase12/Alumno.java
@@ -0,0 +1,15 @@
+package clase12;
+
+public class Alumno extends Persona {
+
+    private float notaPrimerParcial;
+
+    private float notaSegundoParcial;
+
+    public float promedio() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mostrar() {
+    }
+}
---
+++ src/clase12/Persona.java
@@ -0,0 +1,11 @@
+package clase12;
+
+public class Persona {
+
+    private int dni;
+
+    private String nombre;
+
+    public void mostrar() {
+    }
+}
